43d6c94832002ad59135c3ed3d2c9e708797408c
Noah Saria <127628078+noahdsaria@users.noreply.github.com>
2023-09-30 23:06:56 -0500

    Fixed slowdown/crashing bug when adjusting slider

--- scripts.js
+++ scripts.js
@@ -1,26 +1,65 @@
+// Create grid
 let grid = document.getElementById("grid-container");
+let slider = document.querySelector("#size");
+let displayText = document.querySelector("#displayVal");
+let boxes = [];
 
-let createDiv = function () {
+let createDiv = function (numSize = 256) {
     let newDiv;
-    for(let i = 0; i < 256; ++i){
+
+    // Remove old grid children before creating new one to avoid crashing browser
+    boxes.forEach((box) => {
+        grid.removeChild(box); 
+    });
+
+    // Remove old event listener to avoid slowdown
+    grid.removeEventListener('mouseover', handleMouseOver);
+
+    // Set to empty array before iterating
+    boxes = [];
+
+    // Create the grid-boxes and append to container make sure push to empty array for below
+    for(let i = 0; i < numSize; ++i){
         newDiv = document.createElement("div");
         grid.appendChild(newDiv);
         newDiv.classList.add("grid-box");
+        boxes.push(newDiv);
+    };
+
+    // Use Event delegation, instead of adding event listeners to mitigate slow down from adjusting slider
+    grid.addEventListener('mouseover', handleMouseOver);
+
+};
+
+
+// Event listener function
+let handleMouseOver = function (event) {
+    if (event.target.classList.contains('grid-box')) {
+        changeColor(event.target);
     }
 };
 
-// Make sure to create grid first before adding event listeners!
 createDiv();
 
+
+// Sketch on grid
 let changeColor = function (element) {
     element.style.backgroundColor = 'blue';
 }
 
-const pixels = document.querySelectorAll(".grid-box");
+// Size Slider & Display
+let updateDisplay = function () {
+    displayText.textContent = slider.value + "x" + slider.value;
+};
 
-pixels.forEach((pixel) => {
-    pixel.addEventListener('mouseover', () => {
-    changeColor(pixel);
+slider.addEventListener('input', () => {
+    let newBoxSize = 100 / slider.value + "%";
+    let newGridSize = slider.value * slider.value;
+    updateDisplay();
+    createDiv(newGridSize);
+    boxes.forEach((box) => {
+        box.style.height = newBoxSize;
+        box.style.width = newBoxSize;
     })
 });
 
